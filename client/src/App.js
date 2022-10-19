a60084106d5d7856a01c4e925f420660ba3c801d
sanjay r r <sanjusanjayrr@gmail.com>
2022-10-19 23:18:12 +0530

    19/10/22

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,90 @@
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Transaction from "../../server/models/Transaction.js";
 
 function App() {
-  const [form, setForm] = useState ({
+  const [form, setForm] = useState({
     amount: 0,
-    descrition: "",
+    description: "",
     date: "",
   });
-  async function handleSubmit(e){
-    e.preventDefault();
-    const res = await fetch('http://localhost:4000/transaction' ,{ method: "POST",
-    body:form,});
-    console.log(res);
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  async function fetchTransactions() {
+    const res = await fetch("http://localhost:4000/transaction");
+    const { data } = await res.json();
+    console.log(data);
   }
-  function handleInput(e){
-    setForm({ ...form,[e.target.name]: e.target.value});
+
+  function handleInput(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await fetch("http://localhost:4000/transaction", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
   }
   return (
     <div>
-     <form onSubmit={handleSubmit}> 
+      <form onSubmit={handleSubmit}>
+        <label>amount </label>
 
-     <label>amount </label>  
-
-      <input type= "number" 
-          name= "amount"
+        <input
+          type="number"
+          name="amount"
           value={form.amount}
-          onChange={handleInput}  
+          onChange={handleInput}
           placeholder="Enter transtion amount"
-      /> 
+        />
 
-      <label>Description </label> 
-      <input type= "text" 
-         name = "descrition"
-         value={form.descrition} 
-         onChange={handleInput} 
-         placeholder="Enter transtion descrition"
-      />  
+        <label>Description </label>
+        <input
+          type="text"
+          name="description"
+          value={form.description}
+          onChange={handleInput}
+          placeholder="Enter transtion description"
+        />
 
-      <label>date </label> 
+        <label>date </label>
 
-      <input type= "date" 
-         name = "date"
-         value={form.date}
-         onChange={handleInput}  
-      />  
+        <input
+          type="date"
+          name="date"
+          value={form.date}
+          onChange={handleInput}
+        />
 
-      <button type="submit">Submit</button>
+        <button type="submit">Submit</button>
+      </form>
 
-     </form>
+      <br />
+
+      <section>
+        <table>
+          <thead>
+            <th>Amount</th>
+            <th>Description</th>
+            <th>Date</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>dis</td>
+              <td>amount</td>
+              <td>date</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,18 +1,39 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from "body-parser";
+import Transaction from './models/Transaction.js';
 
 const PORT = 4000;
 const app =  express();
-app.use(cors);
+app.use(cors());
+app.use(bodyParser.json());
 
- await mongoose.connect("mongodb+srv://Sanjay:sanju123@bitfumes-mern.zwxhdmc.mongodb.net/?retryWrites=true&w=majority"
+await mongoose.connect(
+    "mongodb+srv://Sanjay:sanju123@bitfumes-mern.zwxhdmc.mongodb.net/?retryWrites=true&w=majority"
 );
 console.log("MongoDB connection is successful");
 
 app.get("/" , (req, res) => {
     res.send("Hello world");
 });
+
+app.get("/transaction", async (req, res) => {
+    const transaction = await Transaction.find({});
+    res.json({transaction});
+})
+
+app.post("/transaction" ,async (req, res) => {
+    const {amount, description, date } = req.body;
+    const transaction = new Transaction({
+        amount, 
+        description,
+        date,
+    })
+    await transaction.save();
+    res.json({message: "Success"});
+});
+
 
 app.listen(PORT , () => {
    console.log("Server is runing at http://localhost:4000");
